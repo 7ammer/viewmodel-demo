4022d24fe5aa51e56d2c5ff1cae26c3ac8050e62
James Isbell <james@isbell.net>
2016-03-12 13:41:04 +0000

    Manuel's version

--- viewmodel-demo.js
+++ viewmodel-demo.js
@@ -1,35 +1,57 @@
 if (Meteor.isClient) {
 
-    var localColl;
-    function myLocalCollection(){
-        localColl = new Mongo.Collection(null);
-        localColl.insert({item:"foo", foo:{title:"hello moon", alignment: "left"}});
-        localColl.insert({item:"bar", bar:{text:"What's your favorite bar?", chocolate:"Twix"}});
-        localColl.insert({item:"foo", foo:{title:"hello sun", alignment: "top"}});
-        return localColl.find({});
-    };
+  localColl = new Mongo.Collection(null);
+  localColl.insert({item:"foo", itemData:{title:"hello moon", alignment: "left"}});
+  localColl.insert({item:"bar", itemData:{text:"What's your favorite bar?", chocolate:"Twix"}});
+  localColl.insert({item:"foo", itemData:{title:"hello sun", alignment: "top"}});
 
-    Template.demoVM.viewmodel({
-        title: "hello world",
-        array: myLocalCollection(),
-        loadSampleData: function() {
-            this.load(myLocalCollection());
-        },
-        events: {
-            'click #submit': function(event, templateInstance) {
-                var obj = this.data();
-                obj.array = obj.array.fetch();
-                console.log(obj);
-            },
-            'click [data-add]': function(evt, templateInstance) {
-                localColl.insert({item: evt.currentTarget.value});
-            },
-            'click [move-up]': function(evt, templateInstance) {
-                console.log(evt, templateInstance);
-            },
-            'click [move-down]': function(evt, templateInstance) {
-                console.log(evt, templateInstance);
-            },
+  Template.demoVM.viewmodel({
+    title: "hello world",
+    collection() {
+      return localColl.find();
+    },
+    submit(){
+      alert("Submit!");
+      console.log(localColl.find({}).fetch());
+    },
+    addFoo(){
+      localColl.insert({
+        item: 'foo',
+        itemData: {
+          title: '',
+          alignment: ''
         }
-    });
+      })
+    },
+    addBar(){
+      localColl.insert({
+        item: 'bar',
+        itemData: {
+          text: '',
+          chocolate: ''
+        }
+      })
+    }
+  });
+
+  Template.arrayItem.viewmodel({
+    delete() {
+      localColl.remove({ _id: this._id() });
+    }
+  });
+
+  ViewModel.mixin({
+    saveProps: {
+      save() {
+        localColl.update(this.parent()._id(), { $set: { itemData: this.data() }});
+      }
+    }
+  })
+
+  Template.foo.viewmodel({
+    mixin: 'saveProps'
+  });
+  Template.bar.viewmodel({
+    mixin: 'saveProps'
+  });
 }
