6e0e0908951e61a4f6930ce2a94325a99269d5df
James Isbell <james@isbell.net>
2016-03-13 20:28:21 +0000

    update

--- viewmodel-demo.js
+++ viewmodel-demo.js
@@ -16,6 +16,30 @@ JSON.unflatten = function(data) {
     }
     return resultholder[""] || resultholder;
 };
+JSON.flatten = function(data) {
+    var result = {};
+    function recurse (cur, prop) {
+        if (Object(cur) !== cur) {
+            result[prop] = cur;
+        } else if (Array.isArray(cur)) {
+             for(var i=0, l=cur.length; i<l; i++)
+                 recurse(cur[i], prop + "[" + i + "]");
+            if (l == 0)
+                result[prop] = [];
+        } else {
+            var isEmpty = true;
+            for (var p in cur) {
+                isEmpty = false;
+                recurse(cur[p], prop ? prop+"."+p : p);
+            }
+            if (isEmpty && prop)
+                result[prop] = {};
+        }
+    }
+    recurse(data, "");
+    return result;
+}
+
 function createTemplateObjFromSchema(schema, pick){
     var ss;
     if(pick){
@@ -57,61 +81,143 @@ var billboard = new SimpleSchema({
 });
 
 TestSchema = new SimpleSchema({
-    title: {type: String, label:"Title"},
+    title: {type: String, label:"Title", defaultValue:"defaulting"},
+    body: {type: String, label:"Body", defaultValue:"defaulting"},
+    'test.test2': {type: String, label:"test2", defaultValue:"defaulting"},
     layout: {type:[Object],label:"layout"},
     'layout.$.item':{type:String, label: "Comp Name",defaultValue:"hello", allowedValues: ["hero","billboard"]},
     'layout.$.hero':{type:hero, label: "Hero"},
     'layout.$.billboard':{type:billboard, label: "Billboard"}
 });
 
-if (Meteor.isClient) {
-    localColl = new Mongo.Collection(null);
-    localColl.insert({item:"hero", hero:{title:"hello moon", alignment: "left"}});
-    localColl.insert({item:"billboard", billboard:{title:"What's your favorite bar?", background:"Twix"}});
-    localColl.insert({item:"hero", hero:{title:"hello sun", alignment: "top"}});
 
-    Template.dynamicForm.viewmodel({
-        title: "hello world",
-        collection() {
-          return localColl.find();
+if (Meteor.isClient) {
+    Template.registerHelper("log", function(argument){
+        console.log("log: ", argument);
+    });
+
+    // localColl = new Mongo.Collection(null);
+    // localColl.insert({item:"hero", hero:{title:"hello moon", alignment: "left"}});
+    // localColl.insert({item:"billboard", billboard:{title:"What's your favorite bar?", background:"Twix"}});
+    // localColl.insert({item:"hero", hero:{title:"hello sun", alignment: "top"}});
+
+    Pages = new Mongo.Collection(null);
+    Pages.insert({
+        _id:"khds89g8",
+        title: "hello james",
+        body: "body text here",
+        test:{
+            test2: "yo yo"
+        },
+        layout:[
+            {_id:"gfds8089", item:"hero", hero:{title:"hello moon", alignment: "left"}},
+            {_id:"jhds8908", item:"billboard", billboard:{title:"What's your favorite bar?", background:"Twix"}},
+            {_id:"h8g9f090", item:"hero", hero:{title:"hello sun", alignment: "top"}}
+        ]
+    });
+
+    ViewModel.share({
+        pages:{
+            testing123: "this is a demo test that will be interesting if it works",
+            formDataFlattened: JSON.flatten(Pages.findOne({})),
+            formData: Pages,
+            schema: TestSchema
+        },
+        utils:{
+            val(field){
+                return this.formDataFlattened()[field];
+            },
+            ssLabel: function(){
+                var field = this.templateInstance.data.field;
+                return field, this.schema()._schema[field].label;
+            },
+            ssDefaultValue: function(liveValue){
+                if(liveValue){
+                    return liveValue;
+                }else{
+                    var field = this.templateInstance.data.field;
+                    return this.schema()._schema[field].defaultValue || "";
+                }
+            }
+        }
+    });
+    Template.demoVM.viewmodel({
+        onCreated: function() {
+            this.load({share:[this.templateInstance.data.shared, 'utils']});
+        }
+    });
+
+    Template.basicForm.viewmodel({
+        onCreated: function() {
+            this.load({share:[this.templateInstance.data.shared, 'utils']});
         },
         submit(){
-          console.log(localColl.find({}).fetch());
+          console.log(this.data(),this.formData().findOne({}));
+          event.preventDefault();
+        }
+    });
+
+    Template.textInput.viewmodel({
+        onCreated: function() {
+            this.load({share:[this.templateInstance.data.shared, 'utils']});
         },
-        add(comp, schemaName){
+        // onRendered(){
+        //     console.log(this);
+        // }
+    });
+
+    Template.textareaInput.viewmodel({
+        onCreated: function() {
+            this.load({share:[this.templateInstance.data.shared, 'utils']});
+        }
+    });
+
+    //TODO on create of this template create a localCollection and add only the needed field into it
+        // this.formData().find({},{fields:{layout:1}})
+
+    Template.dynamicForm.viewmodel({
+        onCreated: function() {
+            this.load({share:[this.templateInstance.data.shared, 'utils']});
+        },
+        collection() {
+          return this.formData().findOne({},{fields:{layout:1}}).layout;
+        },
+        add(comp, schemaName,test){
             var objTml = createTemplateObjFromSchema(TestSchema, schemaName).layout[0][comp];
 
             localColl.insert({
                 item: comp,
                 [comp]: objTml
             });
+            event.preventDefault();
         }
     });
+
 
     Template.arrayItem.viewmodel({
         delete() {
-            localColl.remove({ _id: this._id() });
+            event.preventDefault();
+            var formDataId = this.parent().formData().findOne({})._id;
+
+            this.parent().formData().update({_id: formDataId}, {$pull: {
+                'layout': {'_id': this._id()}
+            }});
         },
-        getData(id){
-            var d = localColl.findOne({_id:id});
-            console.log(d);
+        getData(d){
+            console.log(d[d.item]);
             return d[d.item];
         },
-    });
+        //TODO get data and attach back to main dataStorage: this.formData()
+        //TODO make saving automatic with events (keyup, input,change etc). Make sure its throttled
+        save() {
+            event.preventDefault();
+            var formDataId = this.parent().formData().findOne({})._id;
+            var d = this.data();
+            d[d.item] = this.child(this.item()).data();
 
-    ViewModel.mixin({
-        saveProps: {
-            save(name) {
-                localColl.update(this.parent()._id(), { $set: { [name]: this.data() }});
-            }
+            this.parent().formData().update({_id:formDataId, 'layout._id': d._id}, {$set:{
+                'layout.$': d
+            }});
         }
-    })
-
-    Template.hero.viewmodel({
-        mixin: 'saveProps'
-    });
-
-    Template.billboard.viewmodel({
-        mixin: 'saveProps'
     });
 }
